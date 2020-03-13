0c3118aae85ea9546ecb82c0b06277519c90bda2
Chris Underwood <cunderwoodmn@gmail.com>
2020-03-13 15:26:41 -0400

    add index.js and code top change butoon text depending if staff are logged in.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+
+let status = undefined;
+
+fetch('https://answers.library.brandeis.edu/1.0/chat/widgets/status/9616')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+
+    status = data.online;
+
+    if (status === true) {
+      document.getElementById("lcs_slide_out_button-9616").innerText = "Chat with a Librarian"
+    } else {
+      document.getElementById("lcs_slide_out_button-9616").innerText = "Ask a Librarian"
+    }
+
+  });
+
